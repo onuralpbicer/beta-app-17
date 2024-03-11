89661bb570d9bd0c77d3450dc1c49bb6c19aa59a
Onur Alp Bicer <onuralpbicer@gmail.com>
2024-03-11 13:14:20 +1100

    Added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.betamuhendislik.asset.app',
+  appName: 'beta-app',
+  webDir: 'dist/beta-app/browser',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
