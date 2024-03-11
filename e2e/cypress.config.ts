561a011316f5664d161812b7a97ca7a3d1fd6e65
Onur Alp Bicer <onuralpbicer@gmail.com>
2024-03-11 12:55:23 +1100

    Initial commit

---
+++ e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run beta-app:serve:development',
+        production: 'nx run beta-app:serve:production',
+      },
+      ciWebServerCommand: 'nx run beta-app:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
