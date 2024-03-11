bc38337366fb53827c4701460ef96042043d1d37
Onur Alp Bicer <onuralpbicer@gmail.com>
2024-03-11 13:51:30 +1100

    Set up login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import {
+    IonButton,
+    IonCard,
+    IonContent,
+    IonInput,
+    IonSpinner,
+} from '@ionic/angular/standalone'
 
 @Component({
     selector: 'beta-app-login',
     standalone: true,
-    imports: [CommonModule],
+    imports: [
+        IonContent,
+        CommonModule,
+        ReactiveFormsModule,
+        IonCard,
+        IonInput,
+        IonButton,
+        IonSpinner,
+    ],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+    private fb = inject(FormBuilder)
+
+    public form = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+    })
+
+    async handleSubmit() {
+        if (this.form.invalid) return
+
+        console.log(this.form.value)
+    }
+}
