5ec7795d9cb44ba013aaee9c57bfd0eb61a4a3cd
Onur Alp Bicer <onuralpbicer@gmail.com>
2024-03-11 13:15:26 +1100

    Prettier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.betamuhendislik.asset.app',
-  appName: 'beta-app',
-  webDir: 'dist/beta-app/browser',
-  server: {
-    androidScheme: 'https',
-  },
-};
+    appId: 'com.betamuhendislik.asset.app',
+    appName: 'beta-app',
+    webDir: 'dist/beta-app/browser',
+    server: {
+        androidScheme: 'https',
+    },
+}
 
-export default config;
+export default config
