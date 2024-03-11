c30c1dff4d43ac66609bb900ceb9b59877d1e37f
Onur Alp Bicer <onuralpbicer@gmail.com>
2024-03-11 20:14:58 +1100

    Tidy up home page

--- src/app/home/home-page.component.ts
+++ src/app/home/home-page.component.ts
@@ -1,11 +1,33 @@
 import { Component, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AuthService } from '../shared/auth.service'
+import {
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonIcon,
+} from '@ionic/angular/standalone'
+import { addIcons } from 'ionicons'
+import { logOutOutline } from 'ionicons/icons'
+
+addIcons({
+    logOutOutline,
+})
 
 @Component({
     selector: 'beta-app-home-page',
     standalone: true,
-    imports: [CommonModule],
+    imports: [
+        IonIcon,
+        IonButton,
+        IonButtons,
+        IonToolbar,
+        IonHeader,
+        IonContent,
+        CommonModule,
+    ],
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.scss',
 })
