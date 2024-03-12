75a68b8d50f8f8d66f4c8eefb7b2eef21c35ec74
Onur Alp Bicer <onuralpbicer@gmail.com>
2024-03-12 19:54:16 +1100

    A bit of error handling

--- src/app/shared/equipments.service.ts
+++ src/app/shared/equipments.service.ts
@@ -6,7 +6,7 @@ import {
     IEquipmentTypeFields,
     IEquipmentFields,
 } from './contentful'
-import { merge, pick } from 'rambda'
+import { isNil, merge, pick } from 'rambda'
 import { ListPage } from './model'
 
 @Injectable({
@@ -18,27 +18,42 @@ export class EquipmentsService {
     async getEquipmentList(
         id: string = IContentfulContent.EquipmentTypeList,
     ): Promise<ListPage> {
-        const listEntry = await this.syncService.getEntry<
-            IEquipmentTypeListFields | IEquipmentTypeFields
-        >(id)
+        try {
+            const listEntry = await this.syncService.getEntry<
+                IEquipmentTypeListFields | IEquipmentTypeFields
+            >(id)
 
-        const itemEntries = await Promise.all(
-            listEntry.fields.items.map((item) =>
-                this.syncService.getEntry<
-                    IEquipmentTypeFields | IEquipmentFields
-                >(item.sys.id),
-            ),
-        )
+            if (isNil(listEntry))
+                return {
+                    name: '',
+                    items: [],
+                }
 
-        const items = itemEntries.map((item) =>
-            merge(pick(['id'], item.fields), {
-                link: item.sys.id,
-            }),
-        )
+            const itemEntries = await Promise.all(
+                listEntry.fields.items.map((item) =>
+                    this.syncService.getEntry<
+                        IEquipmentTypeFields | IEquipmentFields
+                    >(item.sys.id),
+                ),
+            )
 
-        return {
-            name: listEntry.fields.id,
-            items,
+            const items = itemEntries.map((item) =>
+                merge(pick(['id'], item.fields), {
+                    link: item.sys.id,
+                }),
+            )
+
+            return {
+                name: listEntry.fields.id,
+                items,
+            }
+        } catch (error) {
+            console.log(error)
+
+            return {
+                name: '',
+                items: [],
+            }
         }
     }
 
